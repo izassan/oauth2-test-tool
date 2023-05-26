8b2606b74f397b58ca276af44657e8ad07a5eb98
izassan <izassan.si@gmail.com>
2023-05-26 10:15:01 +0900

    add: added config parameter and tag
    added a parameter for using credential file
    added tag for json

--- types/type_config.go
+++ types/type_config.go
@@ -1,12 +1,13 @@
 package types
 
 type OttConfig struct{
-    Provider string
-    Auth_flow string
-    Auth_uri string
-    Token_uri string
-    Client_id string
-    Client_secret string
-    Scope string
-    Require_string string
+    Provider string `json:"provider"`
+    Auth_flow string `json:"auth_flow"`
+    Auth_uri string `json:"auth_uri"`
+    Token_uri string `json:"token_uri"`
+    Client_id string `json:"client_id"`
+    Client_secret string `json:"client_secret"`
+    CredentialFile string `json:"credential_file"`
+    Scope string `json:"scope"`
+    Require_string string `json:"require_string"`
 }
