3639c36d26f60b7ed9d9c0cf9f7ecc5190a497b5
izassan <izassan.si@gmail.com>
2023-05-29 22:13:29 +0900

    add: added dummy processes for azuread

--- flows/authorization_code_azuread.go
+++ flows/authorization_code_azuread.go
@@ -1,1 +1,25 @@
 package flows
+
+import (
+	"errors"
+
+	"github.com/izassan/oauth2-testtool/types"
+)
+
+type azureADAuthorizer struct{}
+
+func (a *azureADAuthorizer) init(config *types.OttConfig) error{
+    return errors.New("Unimplemented")
+}
+
+func (a *azureADAuthorizer) GenerateAuthorizeURL(config *types.OttConfig) (string, error){
+    return "", errors.New("Unimplemented")
+}
+
+func (a *azureADAuthorizer) ExchangeAccessToken(authorizationCode string) (*token, error){
+    return nil, errors.New("Unimplemented")
+}
+
+func (a *azureADAuthorizer) ValidateState(state string) error{
+    return errors.New("Unimplemented")
+}
