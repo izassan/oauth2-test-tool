3f72870190a101cb0e553311169fb99163bd4cdc
izassan <izassan.si@gmail.com>
2023-06-27 22:36:30 +0900

    update: only generate config structure

--- config/config.go
+++ config/config.go
@@ -5,57 +5,55 @@ import (
 	"errors"
 	"net/http"
 	"os"
+
+	"github.com/izassan/oidc-testing-tool/flag"
 )
 
 const RESPONSETYPE_CODE= "code"
-const KEY_PROVIDER = "provider"
 
-type Config interface {
-    GetFullAuthorizeURI() (string, error)
-    CreateSecurityParams() error
-    GetTokenRequest() (*http.Request, error)
-}
+type Json map[string]interface{}
+type Config interface {}
 
-func New(jsonFilePath string) (Config, error) {
+func New(jsonFilePath string, flags *flag.Flags) (Config, error) {
     fileBytes, err := readFile(jsonFilePath)
     if err != nil{
         return nil, err
     }
 
-    provider, err := getProvider(fileBytes)
+    var jsonObj Json
+    if err := json.Unmarshal(fileBytes, &jsonObj); err != nil{
+        return nil, errors.New("failed mapping from json")
+    }
+    provider, err := getProvider(jsonObj)
     if err != nil{
         return nil, err
     }
+
     cfg, err := getConfigByProvider(provider, fileBytes)
     if err != nil{
         return nil, err
     }
+
     return cfg, nil
 }
 
-func getProvider(fileBytes []byte) (string, error){
+func getProvider(j Json) (string, error){
+    const KEY_PROVIDER = "provider"
     FailedGetProviderError := errors.New("failed get provider from json")
-    FailedMappingError := errors.New("failed mapping from json")
 
-    var rawJson map[string]interface{}
-    err := json.Unmarshal(fileBytes, &rawJson)
-    if err != nil{
-        return "", FailedMappingError
-    }
-
-    provider, exist := rawJson[KEY_PROVIDER]
+    provider, exist := j[KEY_PROVIDER]
     if !exist{
         return "", FailedGetProviderError
     }
     return provider.(string), nil
 }
 
-func getConfigByProvider(provider string, fileBytes []byte) (Config, error){
+func getConfigByProvider(provider string, jsonBytes []byte) (Config, error){
     UnsupportProviderError := errors.New("unsupport provider")
     switch (provider){
     case GOOGLE_PROVIDER_NAME:
         var cfg GoogleOTTConfig
-        if err := json.Unmarshal(fileBytes, &cfg); err != nil{
+        if err := json.Unmarshal(jsonBytes, &cfg); err != nil{
             return nil, err
         }
         return cfg, nil
--- config/config_google.go
+++ config/config_google.go
@@ -1,91 +1,35 @@
 package config
 
 import (
-	"bytes"
-	"net/http"
-	"net/url"
-	"reflect"
-
 	"github.com/izassan/oidc-testing-tool/security"
 )
 
 const GOOGLE_PROVIDER_NAME = "google"
-const QUERY_NAME_KEY = "qname"
 
 type GoogleOTTConfig struct{
     AuthFlow string `json:"auth_flow"`
-    ClientId string `json:"client_id" qname:"client_id"`
-    ClientSecret string `json:"client_secret" qname:"client_secret"`
-    Scope string `json:"scope" qname:"scope"`
-    Authorize AuthorizeParams `json:"authorize"`
-    Token TokenParams `json:"token"`
+    ClientId string `json:"client_id"`
+    ClientSecret string `json:"client_secret"`
+    Scope string `json:"scope"`
+    Authorize GoogleAuthorizeParams `json:"authorize"`
+    Token GoogleTokenParams `json:"token"`
     JwkURI string `json:"jwk_uri"`
     SecurityParams *security.SecurityParams
 }
 
-type AuthorizeParams struct{
+type GoogleAuthorizeParams struct{
     AuthorizeURI string `json:"auth_uri"`
-    AuthorizeOptions AuthorizeOptions `json:"options"`
+    AuthorizeOptions GoogleAuthorizeOptions `json:"options"`
 }
 
-type AuthorizeOptions struct{
-    AccessType string `json:"access_type" qname:"access_type"`
-    Display string `json:"display" qname:"display"`
-    Hd string `json:"hd" qname:"hd"`
-    LoginHint string `json:"login_hint" qname:"login_hint"`
-    Prompt string `json:"prompt" qname:"prompt"`
+type GoogleAuthorizeOptions struct{
+    AccessType string `json:"access_type"`
+    Display string `json:"display"`
+    Hd string `json:"hd"`
+    LoginHint string `json:"login_hint"`
+    Prompt string `json:"prompt"`
 }
 
-type TokenParams struct{
+type GoogleTokenParams struct{
     TokenURI string `json:"token_uri"`
-}
-
-func (c GoogleOTTConfig) GetFullAuthorizeURI() (string, error){
-    authorizeUri, err := url.Parse(c.Authorize.AuthorizeURI)
-    if err != nil{
-        return "", err
-    }
-    q := authorizeUri.Query()
-    addQueries(c, &q)
-    addQueries(c.Authorize.AuthorizeOptions, &q)
-
-    authorizeUri.RawQuery = q.Encode()
-    return authorizeUri.String(), nil
-}
-
-func (c GoogleOTTConfig) GetTokenRequest() (*http.Request, error){
-    q := url.Values{}
-    // TODO: add queries
-    // required: code, client_id, client_secret, grant_type
-    // optional: code_verifier
-
-    req, err := http.NewRequest("POST", c.Token.TokenURI, bytes.NewReader([]byte(q.Encode())))
-    if err != nil{
-        return nil, err
-    }
-    return req, nil
-}
-
-func (c GoogleOTTConfig) CreateSecurityParams() error{
-    sp, err := security.NewSecurityParams()
-    if err != nil{
-        return err
-    }
-    c.SecurityParams = sp
-    return nil
-}
-
-func addQueries(o any, q *url.Values){
-    refValues := reflect.ValueOf(o)
-    refTypes := refValues.Type()
-    for i:=0; i < refValues.NumField(); i++{
-        field := refTypes.Field(i)
-        value := refValues.FieldByName(field.Name).Interface()
-        if _, ok := value.(string); !ok || value == ""{
-            continue
-        }
-        if tagValue, ok := field.Tag.Lookup(QUERY_NAME_KEY); ok{
-            q.Add(tagValue, value.(string))
-        }
-    }
 }
